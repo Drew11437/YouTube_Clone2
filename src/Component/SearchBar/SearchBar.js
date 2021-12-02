86641fa622cc90e2ba7487be7f7ba5d5042a9192
unknown <drew17509@gmail.com>
2021-12-02 14:05:14 -0700

    searchBar update

--- src/Component/SearchBar/SearchBar.js
+++ src/Component/SearchBar/SearchBar.js
@@ -4,18 +4,34 @@ import "./SearchBar.css"
 
 class SearchBar extends React.Component {
     state = {
-        searchTerm: '',
+        searchTerm: "paaR5szOfKU"
+    };
+    handleChange = (event) => {
+        this.setState({
+        searchTerm: event.target.value
+        });
+
     }
+        handleSubmit = event => {
+        event.preventDefault();
+        this.props.newVideo(this.state.searchTerm);
+
+    }
+
     render (){
         return(
             <div class="col-md-6">
-           
+               <form onSubmit={this.handleSubmit}> 
            
             <label for="search" class="form-label">Search </label>
-            <input type="search" class="form-control" id="input"></input>
-            
+            <input type="text" placeholder ="search..."onChange={this.handleChange}/>
+            </form>
           </div>
         )
     }
+
+    
 }
+
+
 export default SearchBar;
